0a9f34faaaacb9254b81cc6fd523e0be3bb0b09d
조경진 <cho881020@gmail.com>
2021-08-07 14:59:52 +0900

    로그인 버튼 눌리면 아이디/비번 추출

--- app/src/main/java/com/neppplus/colosseum_20210807/MainActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20210807/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -15,6 +16,14 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        loginBtn.setOnClickListener {
+            val inputId = idEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+
+//            이 아이디,비번을 들고 => 서버에 진짜 회원이 맞는지 문의. (로그인)
+
+        }
 
 
     }
