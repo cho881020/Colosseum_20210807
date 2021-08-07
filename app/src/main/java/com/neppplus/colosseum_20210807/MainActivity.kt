53abfe42e69e34de4be2a1f7645036f2e4988f7b
조경진 <cho881020@gmail.com>
2021-08-07 14:58:01 +0900

    로그인 화면 UI작업

--- app/src/main/java/com/neppplus/colosseum_20210807/MainActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20210807/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.neppplus.colosseum_20210807
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : BaseActivity() {
 
@@ -13,6 +15,7 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
 
     }
 
