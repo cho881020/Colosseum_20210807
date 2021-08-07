72d2bccf9939f46ab89747e72b6f0e1dd4cf677a
조경진 <cho881020@gmail.com>
2021-08-07 14:48:46 +0900

    BaseActivity 추가 + 메인화면에서 상속받음

---
+++ app/src/main/java/com/neppplus/colosseum_20210807/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.neppplus.colosseum_20210807
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
