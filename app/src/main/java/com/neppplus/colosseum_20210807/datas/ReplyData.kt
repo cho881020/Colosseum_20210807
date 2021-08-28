90276630feac9bd81d5ff1d793b37bba295c2176
조경진 <cho881020@gmail.com>
2021-08-28 15:20:22 +0900

    서버가 알려주는 작성일시를 댓글의 작성일시 (Calendar) 형태로 저장

--- app/src/main/java/com/neppplus/colosseum_20210807/datas/ReplyData.kt
+++ app/src/main/java/com/neppplus/colosseum_20210807/datas/ReplyData.kt
@@ -1,6 +1,8 @@
 package com.neppplus.colosseum_20210807.datas
 
 import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ReplyData {
 
@@ -17,6 +19,8 @@ class ReplyData {
 //    선택된 진영을 selectedSide String으로 저장.
     var selectedSide = ""
 
+//    글이 작성된 일시를 Calendar 형태로 저장.
+    val createdAt = Calendar.getInstance()  // 기본값 : 현재 시간이 자동으로 기록됨. -> 서버에서 알려주는 댓글 작성 일시로 데이터 변경.
 
     companion object {
 
@@ -38,6 +42,18 @@ class ReplyData {
             val selectedSideObj = jsonObj.getJSONObject("selected_side")
 
             reply.selectedSide = selectedSideObj.getString("title")
+
+
+//            서버가 알려주는 작성 일시 (String) 를 받아서 -> SimpleDateFormat 이용 -> Calendar형태로 담자.
+
+            val sdf = SimpleDateFormat( "yyyy-MM-dd HH:mm:ss" )
+
+            val serverTimeStr = jsonObj.getString("created_at")
+
+
+//            sdf로 서버시간Str 파싱 => 댓글의.작성일시의.시간데이터로 대입.
+            reply.createdAt.time = sdf.parse(serverTimeStr)
+
 
             return reply
 
